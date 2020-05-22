a47c8dcfb20cac20b4c4ef562a30dc5aa8218484
wangminwu <wangminwu@zuoyebang.com>
2020-05-22 14:42:08 +0800

    upfate

--- js/website.js
+++ js/website.js
@@ -1,361 +1,373 @@
 const website = [
     {
-        name:'前端相关',
-        children:[
+        name: '前端相关',
+        children: [
             {
-                name:'掘金',
-                url:'https://juejin.im/'
+                name: '掘金',
+                url: 'https://juejin.im/'
             },
             {
-                name:'淘宝前端fed',
-                url:'http://taobaofed.org/'
+                name: '淘宝前端fed',
+                url: 'http://taobaofed.org/'
             },
             {
-                name:'腾讯Alloyteam',
-                url:'http://www.alloyteam.com/'
+                name: '腾讯Alloyteam',
+                url: 'http://www.alloyteam.com/'
             },
             {
-                name:'腾讯 isux',
-                url:'https://isux.tencent.com/'
+                name: '腾讯 isux',
+                url: 'https://isux.tencent.com/'
             },
             {
-                name:'IMWEB',
-                url:'http://imweb.io/'
+                name: 'IMWEB',
+                url: 'http://imweb.io/'
             },
             {
-                name:'百度 fex',
-                url:'http://fex.baidu.com/'
+                name: '百度 fex',
+                url: 'http://fex.baidu.com/'
             },
             {
-                name:'百度 EFE',
-                url:'http://efe.baidu.com/'
+                name: '百度 EFE',
+                url: 'http://efe.baidu.com/'
             },
             {
-                name:'百度 FIS',
-                url:'http://fis.baidu.com/'
+                name: '百度 FIS',
+                url: 'http://fis.baidu.com/'
             },
             {
-                name:'去哪网开发',
-                url:'http://ued.qunar.com/fekit/'
+                name: '去哪网开发',
+                url: 'http://ued.qunar.com/fekit/'
             },
             {
-                name:'菜鸟教程',
-                url:'http://www.runoob.com/'
+                name: '菜鸟教程',
+                url: 'http://www.runoob.com/'
             },
             {
-                name:'张鑫旭',
-                url:'https://www.zhangxinxu.com/'
+                name: '张鑫旭',
+                url: 'https://www.zhangxinxu.com/'
             },
             {
-                name:'阮一峰',
-                url:'http://www.ruanyifeng.com/home.html'
+                name: '阮一峰',
+                url: 'http://www.ruanyifeng.com/home.html'
             },
             {
-                name:'大数据过往记忆',
-                url:'https://www.iteblog.com/'
+                name: '大数据过往记忆',
+                url: 'https://www.iteblog.com/'
             },
             {
-                name:'极客学院 wiki',
-                url:'http://wiki.jikexueyuan.com/'
+                name: '极客学院 wiki',
+                url: 'http://wiki.jikexueyuan.com/'
             },
             {
-                name:'前端工程模块化',
-                url:'http://fex.baidu.com/blog/2014/03/fis-module/'
+                name: '前端工程模块化',
+                url: 'http://fex.baidu.com/blog/2014/03/fis-module/'
             },
             {
-                name:'html5tricks',
-                url:'ttps://www.html5tricks.com/'
+                name: 'html5tricks',
+                url: 'ttps://www.html5tricks.com/'
             },
             {
-                name:'MDN',
-                url:'https://developer.mozilla.org/zh-CN/'
+                name: 'MDN',
+                url: 'https://developer.mozilla.org/zh-CN/'
             },
             {
-                name:'github',
-                url:'https://github.com/'
+                name: 'github',
+                url: 'https://github.com/'
             },
             {
-                name:'css88',
-                url:'http://www.css88.com/'
+                name: 'css88',
+                url: 'http://www.css88.com/'
             },
             {
-                name:'w3cplus',
-                url:'http://www.w3cplus.com/'
+                name: 'w3cplus',
+                url: 'http://www.w3cplus.com/'
             },
             {
-                name:'w3help',
-                url:'http://www.w3help.org/zh-cn/kb/'
+                name: 'w3help',
+                url: 'http://www.w3help.org/zh-cn/kb/'
             },
             {
-                name:'html5plus',
-                url:'http://www.html5plus.org/doc/h5p.html'
+                name: 'html5plus',
+                url: 'http://www.html5plus.org/doc/h5p.html'
             }
 
         ]
     },
     {
-        name:'交互设计',
-        children:[
+        name: '交互设计',
+        children: [
             {
-                name:'web 安全色',
-                url:'http://www.bootcss.com/p/websafecolors/'
+                name: 'web 安全色',
+                url: 'http://www.bootcss.com/p/websafecolors/'
             },
             {
-                name:'颜色',
-                url:'https://sjolzy.cn/html/Nice_color.html'
+                name: '颜色',
+                url: 'https://sjolzy.cn/html/Nice_color.html'
             },
             {
-                name:'sketch 中文网',
-                url:'http://www.sketchcn.com/'
+                name: 'sketch 中文网',
+                url: 'http://www.sketchcn.com/'
             },
             {
-                name:'花瓣',
-                url:'http://huaban.com/'
+                name: '花瓣',
+                url: 'http://huaban.com/'
             },
             {
-                name:'千图网',
-                url:'http://www.58pic.com/'
+                name: '千图网',
+                url: 'http://www.58pic.com/'
             },
 
             {
-                name:'懒人图库',
-                url:'http://www.lanrentuku.com/'
+                name: '懒人图库',
+                url: 'http://www.lanrentuku.com/'
             },
             {
-                name:'17素材',
-                url:'http://www.17sucai.com/'
+                name: '17素材',
+                url: 'http://www.17sucai.com/'
             },
             {
-                name:'literally canvas',
-                url:'http://literallycanvas.com/'
+                name: 'literally canvas',
+                url: 'http://literallycanvas.com/'
             },
             {
-                name:'创客贴',
-                url:'https://www.chuangkit.com/designtools/designindex'
+                name: '创客贴',
+                url: 'https://www.chuangkit.com/designtools/designindex'
             },
             {
-                name:'UI中国',
-                url:'http://www.ui.cn/'
+                name: 'UI中国',
+                url: 'http://www.ui.cn/'
             },
             {
-                name:'UI 设计导航',
-                url:'http://hao.xueui.cn/wcat/xinshou'
-            },{
-                name:'fontello',
-                url:'http://fontello.com/'
-            },{
-                name:'iconfont',
-                url:'https://www.iconfont.cn/'
-            },{
-                name:'fontawesome',
-                url:'https://fontawesome.dashgame.com/'
+                name: 'UI 设计导航',
+                url: 'http://hao.xueui.cn/wcat/xinshou'
+            }, {
+                name: 'fontello',
+                url: 'http://fontello.com/'
+            }, {
+                name: 'iconfont',
+                url: 'https://www.iconfont.cn/'
+            }, {
+                name: 'fontawesome',
+                url: 'https://fontawesome.dashgame.com/'
+            },
+            {
+                name: 'our Sketch',
+                url: 'https://oursketch.com/'
             },
 
         ]
     },
     {
-        name:'教育相关',
-        children:[
+        name: '教育相关',
+        children: [
             {
-                name:'研招网',
-                url:'http://yz.chsi.com.cn/'
+                name: '研招网',
+                url: 'http://yz.chsi.com.cn/'
             },
             {
-                name:'学信网',
-                url:'http://www.chsi.com.cn/'
+                name: '学信网',
+                url: 'http://www.chsi.com.cn/'
             }
         ]
     },
     {
-        name:'技术官网',
-        children:[
+        name: '技术官网',
+        children: [
             {
-                name:'微信小程序',
-                url:'https://mp.weixin.qq.com/cgi-bin/wx'
+                name: '微信小程序',
+                url: 'https://mp.weixin.qq.com/cgi-bin/wx'
             },
             {
-                name:'支付宝小程序',
-                url:'https://opendocs.alipay.com/'
+                name: '支付宝小程序',
+                url: 'https://opendocs.alipay.com/'
             },
             {
-                name:'字节跳动小程序',
-                url:'https://developer.toutiao.com/'
+                name: '字节跳动小程序',
+                url: 'https://developer.toutiao.com/'
             },
             {
-                name:'异常监控badjs',
-                url:'http://slides.com/loskael/badjs/fullscreen#/'
+                name: '异常监控badjs',
+                url: 'http://slides.com/loskael/badjs/fullscreen#/'
             },
             {
-                name:'GIS Cesium',
-                url:'https://cesium.com/cesiumjs/'
+                name: 'GIS Cesium',
+                url: 'https://cesium.com/cesiumjs/'
             },
             {
-                name:'GIS Mapbox',
-                url:'https://www.mapbox.com/'
+                name: 'GIS Mapbox',
+                url: 'https://www.mapbox.com/'
             },
             {
-                name:'GIS openLayer',
-                url:'https://openlayers.org/'
+                name: 'GIS openLayer',
+                url: 'https://openlayers.org/'
             },
             {
-                name:'GIS leaflet',
-                url:'https://leafletjs.com/'
+                name: 'GIS leaflet',
+                url: 'https://leafletjs.com/'
             },
             {
-                name:'GIS 高德地图 API',
-                url:'https://lbs.amap.com/'
+                name: 'GIS 高德地图 API',
+                url: 'https://lbs.amap.com/'
             },
             {
-                name:'GIS 百度地图 API',
-                url:'http://lbsyun.baidu.com/'
+                name: 'GIS 百度地图 API',
+                url: 'http://lbsyun.baidu.com/'
             },
             {
-                name:'GIS 腾讯地图 API',
-                url:'https://lbs.qq.com/'
+                name: 'GIS 腾讯地图 API',
+                url: 'https://lbs.qq.com/'
             },
             {
-                name:'GIS @turf/turf',
-                url:'http://turfjs.org/'
+                name: 'GIS @turf/turf',
+                url: 'http://turfjs.org/'
             },
             {
-                name:'GIS mapv',
-                url:'https://mapv.baidu.com/'
+                name: 'GIS mapv',
+                url: 'https://mapv.baidu.com/'
             },
             {
-                name:'热力图 heatmap',
-                url:'https://www.patrick-wied.at/static/heatmapjs/'
+                name: '热力图 heatmap',
+                url: 'https://www.patrick-wied.at/static/heatmapjs/'
             },
             {
-                name:'热力图 clickheat',
-                url:'https://www.dugwood.com/clickheat/index.html'
+                name: '热力图 clickheat',
+                url: 'https://www.dugwood.com/clickheat/index.html'
             },
             {
-                name:'一站式 uni-app',
-                url:'https://uniapp.dcloud.io/'
+                name: '一站式 uni-app',
+                url: 'https://uniapp.dcloud.io/'
             },
             {
-                name:'图表 echarts',
-                url:'https://www.echartsjs.com/zh/index.html'
+                name: '图表 echarts',
+                url: 'https://www.echartsjs.com/zh/index.html'
             },
             {
-                name:'图表 charts',
-                url:'http://chartjs.cn/'
+                name: '图表 charts',
+                url: 'http://chartjs.cn/'
             },
             {
-                name:'图表 highcharts',
-                url:'https://www.highcharts.com.cn/'
+                name: '图表 highcharts',
+                url: 'https://www.highcharts.com.cn/'
             },
             {
-                name:'三维 D3',
-                url:'https://www.d3js.org.cn/'
+                name: '三维 D3',
+                url: 'https://www.d3js.org.cn/'
             },
             {
-                name:'三维 three',
-                url:'https://threejs.org/'
+                name: '三维 three',
+                url: 'https://threejs.org/'
             },
             {
-                name:'框架 vue',
-                url:'https://cn.vuejs.org/'
+                name: '框架 vue',
+                url: 'https://cn.vuejs.org/'
             },
             {
-                name:'elementUI',
-                url:'https://element.eleme.cn/#/zh-CN'
+                name: 'elementUI',
+                url: 'https://element.eleme.cn/#/zh-CN'
             },
             {
-                name:'请求 axios',
-                url:'https://www.kancloud.cn/yunye/axios/234845'
+                name: '请求 axios',
+                url: 'https://www.kancloud.cn/yunye/axios/234845'
             },
             {
-                name:'请求 fdaciuk/ajax',
-                url:'https://github.com/fdaciuk/ajax'
+                name: '请求 fdaciuk/ajax',
+                url: 'https://github.com/fdaciuk/ajax'
             },
             {
-                name:'antDesign',
-                url:'https://ant.design/index-cn'
+                name: 'antDesign',
+                url: 'https://ant.design/index-cn'
             },
             {
-                name:'vant',
-                url:'https://youzan.github.io/vant/#/zh-CN/'
+                name: 'vant',
+                url: 'https://youzan.github.io/vant/#/zh-CN/'
             },
             {
-                name:'iview',
-                url:'http://v1.iviewui.com/'
+                name: 'iview',
+                url: 'http://v1.iviewui.com/'
             },
             {
-                name:'mintUI',
-                url:'http://mint-ui.github.io/#!/zh-cn'
+                name: 'mintUI',
+                url: 'http://mint-ui.github.io/#!/zh-cn'
             },
             {
-                name:'webpack',
-                url:'https://www.webpackjs.com/'
+                name: 'webpack',
+                url: 'https://www.webpackjs.com/'
             },
             {
-                name:'node',
-                url:'https://nodejs.org/zh-cn/docs/'
+                name: 'node',
+                url: 'https://nodejs.org/zh-cn/docs/'
             },
             {
-                name:'express',
-                url:'https://www.expressjs.com.cn/'
+                name: 'express',
+                url: 'https://www.expressjs.com.cn/'
             },
             {
-                name:'koa',
-                url:'https://koa.bootcss.com/'
+                name: 'koa',
+                url: 'https://koa.bootcss.com/'
             },
             {
-                name:'jquery',
-                url:'https://jquery.com/'
+                name: 'jquery',
+                url: 'https://jquery.com/'
             },
             {
-                name:'bootstrap',
-                url:'https://www.bootcss.com/'
+                name: 'bootstrap',
+                url: 'https://www.bootcss.com/'
             },
             {
-                name:'bezierjs',
-                url:'https://github.com/Pomax/bezierjs'
+                name: 'bezierjs',
+                url: 'https://github.com/Pomax/bezierjs'
             },
             {
-                name:'react',
-                url:'https://react.docschina.org/'
+                name: 'react',
+                url: 'https://react.docschina.org/'
             },
             {
-                name:'reactNative',
-                url:'https://reactnative.cn/'
+                name: 'reactNative',
+                url: 'https://reactnative.cn/'
             },
             {
-                name:'electron',
-                url:'https://electronjs.org/'
+                name: 'electron',
+                url: 'https://electronjs.org/'
             },
             {
-                name:'electron-vue',
-                url:'https://simulatedgreg.gitbooks.io/electron-vue/content/cn/'
+                name: 'electron-vue',
+                url: 'https://simulatedgreg.gitbooks.io/electron-vue/content/cn/'
             },
             {
-                name:'devops',
-                url:'https://devops.phodal.com/'
+                name: 'devops',
+                url: 'https://devops.phodal.com/'
             },
             {
-                name:'CMS strapi',
-                url:'https://strapi.io/'
+                name: 'CMS strapi',
+                url: 'https://strapi.io/'
             },
             {
-                name:'GraphQL',
-                url:'https://graphql.org/'
+                name: 'GraphQL',
+                url: 'https://graphql.org/'
             },
             {
-                name:'yapi',
-                url:'http://yapi.demo.qunar.com/'
+                name: 'yapi',
+                url: 'http://yapi.demo.qunar.com/'
             },
             {
-                name:'docsify',
-                url:'https://docsify.js.org/#/?id=docsify'
-            },{
-                name:'请求 mitmproxy',
-                url:' https://www.mitmproxy.org/'
-            },{
-                name:'js中文',
-                url:'https://zh.javascript.info/'
+                name: 'docsify',
+                url: 'https://docsify.js.org/#/?id=docsify'
+            }, {
+                name: '请求 mitmproxy',
+                url: ' https://www.mitmproxy.org/'
+            }, {
+                name: 'js中文',
+                url: 'https://zh.javascript.info/'
+            },
+            {
+                name: 'brain',
+                url: 'https://github.com/BrainJS/brain.js'
+            },
+            {
+                name: 'tensorflow',
+                url: 'https://www.w3cschool.cn/tensorflow_python/'
             }
-           
+
         ]
     },
 ]
